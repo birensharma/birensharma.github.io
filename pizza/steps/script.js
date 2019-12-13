c09b3f50cd6e134c83f17944c1d6dc4c403f5440
Biren Sharma <biren.sharma@st.niituniversity.in>
2019-12-13 02:05:43 -0800

    push

--- pizza/steps/script.js
+++ pizza/steps/script.js
@@ -1,78 +1,72 @@
-//jQuery time
-var current_fs, next_fs, previous_fs; //fieldsets
-var left, opacity, scale; //fieldset properties which we will animate
-var animating; //flag to prevent quick multi-click glitches
 
-$(".next").click(function(){
-	if(animating) return false;
+var current_fs, next_fs, previous_fs;
+var left, opacity, scale;
+var animating;
+let currentIndex = 0
+const TIME = 4
+let currentTime = 0
+// $('.timertext').text(TIME+'s')
+
+
+function goNext() {
+	if (animating) return false;
 	animating = true;
-	
-	current_fs = $(this).parent();
-	next_fs = $(this).parent().next();
-	
-	//activate next step on progressbar using the index of next_fs
-	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-	
-	//show the next fieldset
-	next_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale current_fs down to 80%
+
+	const fs = document.querySelectorAll('fieldset')
+
+	current_fs = $(fs[currentIndex])
+	next_fs = $(fs[currentIndex + 1])
+
+	currentIndex++
+
+	$("#progressbar li").eq(currentIndex).addClass("active");
+
+
+	next_fs.show();
+
+	current_fs.animate({ opacity: 0 }, {
+		step: function (now, mx) {
+
+
 			scale = 1 - (1 - now) * 0.2;
-			//2. bring next_fs from the right(50%)
-			left = (now * 50)+"%";
-			//3. increase opacity of next_fs to 1 as it moves in
+
+			left = (now * 50) + "%";
+
 			opacity = 1 - now;
-			current_fs.css({'transform': 'scale('+scale+')'});
-			next_fs.css({'left': left, 'opacity': opacity});
-		}, 
-		duration: 500, 
-		complete: function(){
+			current_fs.css({ 'transform': 'scale(' + scale + ')' });
+			next_fs.css({ 'left': left, 'opacity': opacity });
+		},
+		duration: 500,
+		complete: function () {
 			current_fs.hide();
 			animating = false;
-		}, 
-		//this comes from the custom easing plugin
+		},
+
 		easing: 'easeOutQuint'
 	});
-});
-
-$(".previous").click(function(){
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	previous_fs = $(this).parent().prev();
-	
-	//de-activate current step on progressbar
-	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-	
-	//show the previous fieldset
-	previous_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale previous_fs from 80% to 100%
-			scale = 0.8 + (1 - now) * 0.2;
-			//2. take current_fs to the right(50%) - from 0%
-			left = ((1-now) * 50)+"%";
-			//3. increase opacity of previous_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({'left': left});
-			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-		}, 
-		duration: 500, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeOutQuint'
-	});
-});
-
-$(".submit").click(function(){
+}
+function start() {
+	if (currentIndex == 5) return
+	setTimeout(() => {
+		goNext()
+		clearInterval()
+		start()
+	}, (TIME) * 1000);
+}
+start()
+// function startTimer() {
+let timer = setInterval(() => {
+	// if (currentIndex == 5) {
+	// 	$('.timertext').text('0s')
+	// 	clearInterval(timer)
+	// }
+	currentTime++
+	$('.timertext').text(currentTime+'s')
+	// if (currentTime == 1) currentTime = TIME + 1
+	// startTimer()
+}, 1000);
+// }
+// startTimer()
+$(".submit").click(function () {
 	return false;
 })
